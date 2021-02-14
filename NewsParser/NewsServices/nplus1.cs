d5e4204d72b944dcd34bd443b1b9b181f916ec95
SiAvMaAnR <samarkin200220022002@gmail.com>
2021-02-14 19:25:21 +0300

    Nplus1, added try catch

--- NewsParser/NewsServices/nplus1.cs
+++ NewsParser/NewsServices/nplus1.cs
@@ -3,6 +3,7 @@ using AngleSharp.Dom;
 using AngleSharp.Io;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -28,47 +29,60 @@ namespace ParserNews.NewsServices
 
         public override async Task<IEnumerable<News>> GetAllNewsAsync()
         {
-            allNews.Clear();
-            //var documentRequest = DocumentRequest.Get(new Url("https://nplus1.ru/theme/coronavirus-history"));
-            var documentRequest = DocumentRequest.Get(new Url("https://nplus1.ru/rubric/medicine"));
-            var document = await context.OpenAsync(documentRequest);
-            if(document.StatusCode != System.Net.HttpStatusCode.OK)
+            IDocument document = null;
+            try
             {
-                Console.WriteLine($"{Name} {document.StatusCode}");
-                return new List<News>();
-            }
-            var tags = document.QuerySelectorAll("article[class*='item-news']");
-            foreach (var item in tags)
-            {
-                var title = item.QuerySelector("a > div.caption > h3").TextContent;
-                var date = item.QuerySelector("a > div.caption > div.date > span").GetAttribute("title");
-                var link = item.QuerySelector("a").GetAttribute("href");
-
-                var documentSite = DocumentRequest.Get(new Url(BaseUrl + link));
-                var doc = await context.OpenAsync(documentSite);
-
-                var teasers = doc.QuerySelector($"div[class*='body']");
-                string teaser = "";
-                foreach (var it in teasers.Children)
+                allNews.Clear();
+                //var documentRequest = DocumentRequest.Get(new Url("https://nplus1.ru/theme/coronavirus-history"));
+                var documentRequest = DocumentRequest.Get(new Url("https://nplus1.ru/rubric/medicine"));
+                document = await context.OpenAsync(documentRequest);
+                if (document.StatusCode != System.Net.HttpStatusCode.OK)
                 {
-                    if (!it.ToHtml().Contains("class"))
+                    Console.WriteLine($"{Name} {document.StatusCode}");
+                    return new List<News>();
+                }
+                var tags = document.QuerySelectorAll("article[class*='item-news']");
+                foreach (var item in tags)
+                {
+                    var title = item.QuerySelector("a > div.caption > h3").TextContent;
+                    var date = item.QuerySelector("a > div.caption > div.date > span").GetAttribute("title");
+                    var link = item.QuerySelector("a").GetAttribute("href");
+
+                    var documentSite = DocumentRequest.Get(new Url(BaseUrl + link));
+                    var doc = await context.OpenAsync(documentSite);
+
+                    var teasers = doc.QuerySelector($"div[class*='body']");
+                    if (teasers == null) continue;
+                    string teaser = "";
+                    foreach (var it in teasers.Children)
                     {
-                        var tmp = Regex.Replace(it.TextContent, @"[\r\n\t]", " ").Trim();
-                        if (tmp.Length > 100)
+                        if (!it.ToHtml().Contains("class"))
                         {
-                            teaser = tmp; break;
+                            var tmp = Regex.Replace(it.TextContent, @"[\r\n\t]", " ").Trim();
+                            if (tmp.Length > 100)
+                            {
+                                teaser = tmp; break;
+                            }
                         }
                     }
+
+                    //Console.WriteLine($" * {title}\n * {teaser}\n * {BaseUrl + link}\n");
+                    if (getDate(date) == DateTime.Today)
+                    {
+                        allNews.Add(new News(title, teaser, BaseUrl + link));
+                    }
                 }
-               
-                //Console.WriteLine($" * {title}\n * {teaser}\n * {BaseUrl + link}\n");
-                if (getDate(date) == DateTime.Today)
-                {
-                    allNews.Add(new News(title, teaser, BaseUrl + link));
-                }
+                Console.WriteLine($"{Name} {allNews.Count}");
+                return allNews;
             }
-            Console.WriteLine($"{Name} {allNews.Count}");
-            return allNews;
+            catch
+            {
+                if (document.StatusCode != HttpStatusCode.OK)
+                {
+                    Console.WriteLine($"{Name} {document.StatusCode}");
+                }
+                return new List<News>();
+            }
         }
     }
 }
