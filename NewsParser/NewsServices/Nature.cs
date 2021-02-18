88dfe12af668c8937bd4259849eb8a7289372541
SiAvMaAnR <samarkin200220022002@gmail.com>
2021-02-18 20:53:12 +0300

    Fix Nature

--- NewsParser/NewsServices/Nature.cs
+++ NewsParser/NewsServices/Nature.cs
@@ -1,7 +1,9 @@
 ﻿using AngleSharp;
+using AngleSharp.Dom;
 using AngleSharp.Io;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,56 +28,68 @@ namespace ParserNews.NewsServices
 
         public override async Task<IEnumerable<News>> GetAllNewsAsync()
         {
-            allNews.Clear();
-            for (int i = 1; ; i++)
+            IDocument document = null;
+            try
             {
-                var documentRequest = DocumentRequest.Get(new Url($"https://www.nature.com/nature/articles?searchType=journalSearch&sort=PubDate&type=news&page={i}"));
-                var document = await context.OpenAsync(documentRequest);
-                if (document.StatusCode != System.Net.HttpStatusCode.OK)
+                allNews.Clear();
+                for (int i = 1; ; i++)
+                {
+                    var documentRequest = DocumentRequest.Get(new Url($"https://www.nature.com/nature/articles?searchType=journalSearch&sort=PubDate&type=news&page={i}"));
+                    document = await context.OpenAsync(documentRequest);
+                    if (document.StatusCode != System.Net.HttpStatusCode.OK)
+                    {
+                        Console.WriteLine($"{Name} {document.StatusCode}");
+                        return new List<News>();
+                    }
+                    var tags = document.QuerySelectorAll("div#content ul[class*='grid-auto-fill'] > li");
+                    foreach (var item in tags)
+                    {
+                        //Console.WriteLine(item.ToHtml());
+                        //Console.WriteLine("******************************************************************************");
+
+                        var date = item.QuerySelector("time").GetAttribute("datetime");
+
+                        if (CorrectDate(date) == DateTime.Today)
+                        {
+
+                            var title = item.QuerySelector("a.text-gray").TextContent.Trim();
+                            var link = item.QuerySelector("a.text-gray").GetAttribute("href");
+
+
+                            var documentSite = DocumentRequest.Get(new Url(BaseUrl + link));
+                            var doc = await context.OpenAsync(documentSite);
+
+                            var teasers = doc.QuerySelectorAll($"div.align-left div.cleared > p");
+                            if (teasers == null) continue;
+                            string teaser = "";
+
+                            foreach (var it in teasers)
+                            {
+                                if (teaser.Length < 300) teaser += it.TextContent.Trim();
+                                else break;
+                            }
+                            if (title != null && teaser != null && link != null)
+                            {
+                                allNews.Add(new News(title, teaser, link));
+                            }
+                            //Console.WriteLine(date + " " + title + "\n" + BaseUrl + link + "\n"+teaser+"\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{Name} {allNews.Count}");
+                            return allNews;
+                        }
+
+                    }
+                }
+            }
+            catch
+            {
+                if (document.StatusCode != HttpStatusCode.OK)
                 {
                     Console.WriteLine($"{Name} {document.StatusCode}");
-                    return new List<News>();
                 }
-                var tags = document.QuerySelectorAll("div#content ul[class*='grid-auto-fill'] > li");
-                foreach (var item in tags)
-                {
-                    //Console.WriteLine(item.ToHtml());
-                    //Console.WriteLine("******************************************************************************");
-
-                    var date = item.QuerySelector("time").GetAttribute("datetime");
-
-                    if (CorrectDate(date) == DateTime.Today)
-                    {
-
-                        var title = item.QuerySelector("a.text-gray").TextContent.Trim();
-                        var link = item.QuerySelector("a.text-gray").GetAttribute("href");
-
-
-                        var documentSite = DocumentRequest.Get(new Url(BaseUrl + link));
-                        var doc = await context.OpenAsync(documentSite);
-
-                        var teasers = doc.QuerySelectorAll($"div.align-left div.cleared > p");
-                        if (teasers == null) continue;
-                        string teaser = "";
-
-                        foreach (var it in teasers)
-                        {
-                            if (teaser.Length < 300) teaser += it.TextContent.Trim();
-                            else break;
-                        }
-                        if (title != null && teaser != null && link != null)
-                        {
-                            allNews.Add(new News(title, teaser, link));
-                        }
-                        //Console.WriteLine(date + " " + title + "\n" + BaseUrl + link + "\n"+teaser+"\n");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{Name} {allNews.Count}");
-                        return allNews;
-                    }
-
-                }
+                return new List<News>();
             }
         }
 
